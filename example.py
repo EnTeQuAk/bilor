514d0ea416ca99f0f5c730fb20a21ca2757b0398
Christopher Grebs <cg@webshox.org>
2014-09-13 22:39:57 +0200

    basic throttling example, off for unittests now

--- example.py
+++ example.py
@@ -1,16 +1,36 @@
+import time
 import logging
 from bilor.core.handler import BilorHandler
 
 logger = logging.getLogger('other.example')
 logger.addHandler(BilorHandler('http://localhost:8000/'))
 
-logger.debug('This is debug')
-logger.warning('This is warning')
-logger.info('This is info')
-logger.error('This is error')
+
+def full():
+    logger.debug('This is debug')
+    logger.warning('This is warning')
+    logger.info('This is info')
+    logger.error('This is error')
 
 
-try:
-    1 / 0
-except Exception:
-    logger.exception('This occurred')
+    try:
+        1 / 0
+    except Exception:
+        logger.exception('This occurred')
+
+print('Initial test')
+
+full()
+
+time.sleep(60)
+
+print('Successful throttling test')
+for idx in range(12):
+    full()
+
+time.sleep(60)
+
+print('Expected to run into throttling')
+for idx in range(24):
+    full()
+
